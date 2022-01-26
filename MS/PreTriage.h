5c8e787f52177c43fbfc586b77328455df0cf490
lingbaoqwe <52990132+lingbaoqwe@users.noreply.github.com>
2022-01-25 21:45:42 -0500

    Update PreTriage.h

--- MS/PreTriage.h
+++ MS/PreTriage.h
@@ -1,23 +1,3 @@
-/* Citation and Sources...
-Final Project Milestone 5
-Module: PreTriage
-Filename: PreTriage.cpp
-Version 1.0
-Author	Bo Wen Hou
-Revision History
------------------------------------------------------------
-Date      Reason
-2020/4/7  Preliminary release
-2020/?/?  Debugged DMA
------------------------------------------------------------
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
------------------------------------------------------------
-OR
------------------------------------------------------------
-Write exactly which part of the code is given to you as help and
-who gave it to you, or from what source you acquired it.
------------------------------------------------------------*/
 #ifndef SDDS_pret_H_
 #define SDDS_pret_H_
 #include "Patient.h"
